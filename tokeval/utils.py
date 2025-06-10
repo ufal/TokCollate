91221a3432cafe857959bd4c5a3f0299f866cdf9
varisd <varis@ufal.mff.cuni.cz>
2025-06-10 16:49:27 +0200

    additional vocab utility functions

--- tokeval/utils.py
+++ tokeval/utils.py
@@ -1,6 +1,7 @@
 import gzip
+import numpy as np
 from pathlib import Path
-from typing import List, TextIO
+from typing import Dict, List, TextIO
 
 
 def open_file(file: Path, mode: str) -> TextIO:
@@ -23,3 +24,28 @@ def file_path(path_str: str) -> Path:
     if not path.exists():
         raise FileNotFoundError(path)
     return path.absolute()
+
+
+def get_vocabulary(corpus: List[str], token_separator: str = " ") -> Dict[str, int]:
+    """TODO"""
+    vocab = {}
+    for line in corpus:
+        line = line.strip()
+        for tok in line.split(token_separator):
+            if tok in vocab:
+                vocab[tok] += 1
+            else:
+                vocab[tok] = 1
+    return vocab
+
+
+def get_unigram_frequencies(corpus: List[str], token_separator: str = " ") -> np.ndarray:
+    """TODO"""
+    vocab = get_vocabulary(corpus, token_separator)
+    return np.array(sorted(vocab.values(), reverse=True))
+
+
+def get_unigram_distribution(corpus: List[str], token_separator: str = " ") -> np.ndarray:
+    """TODO"""
+    unigram_counts = get_unigram_frequencies(corpus, token_separator)
+    return unigram_counts / unigram_counts.sum()
