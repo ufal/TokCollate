d58187bfc58ed991cf03a163a8f1b5c2fceda050
varisd <varis@ufal.mff.cuni.cz>
2025-06-24 11:02:42 +0200

    added bits metric

---
+++ tokeval/metrics/bits.py
@@ -0,0 +1,26 @@
+import logging
+
+import numpy as np
+from attrs import define
+
+from tokeval.metrics import TokEvalMetric, register_metric
+from tokeval.utils import get_unigram_frequencies
+
+logger = logging.getLogger(__name__)
+
+
+@register_metric("bits")
+@define(kw_only=True)
+class BitsMetric(TokEvalMetric):
+    """TODO."""
+
+    def compute(
+        self,
+        data: dict[str, list[str]],
+        system_label: str,
+    ) -> float:
+        corpus = data[system_label]
+        unigram_freqs = get_unigram_frequencies(corpus)
+        vocab_size = unigram_probs.size
+
+        return -unigram_freqs.sum() * np.log2(vocab_size)
